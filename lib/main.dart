bf82055c9dcfab55c5d9d940064b225c10b3e08c
ram-wappnet <ram.wappnet@gmail.com>
2023-03-21 11:13:35 +0530

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:location_buddy/provider/home_view_provider.dart';
+import 'package:location_buddy/provider/splash_view_provider.dart';
 import 'package:location_buddy/utils/routes/routes.dart';
 import 'package:location_buddy/utils/routes/routes_name.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,20 +19,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(430, 932),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            initialRoute: RoutesName.splashView,
-            onGenerateRoute: Routes.generateRoute,
-          );
-        });
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => HomeViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => RemoteConfigProvider(),
+        ),
+      ],
+      child: ScreenUtilInit(
+          designSize: const Size(430, 932),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp(
+              title: 'Flutter Demo',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              initialRoute: RoutesName.splashView,
+              onGenerateRoute: Routes.generateRoute,
+            );
+          }),
+    );
   }
 }
