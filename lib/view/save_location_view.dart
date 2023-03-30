653b1d69c87d49a0fedaaa227c39c78358c23cda
aditya-wappnet <aditya.wappnet@gmail.com>
2023-03-30 10:29:26 +0530

    commit

--- lib/view/save_location_view.dart
+++ lib/view/save_location_view.dart
@@ -43,8 +43,8 @@ class _SaveLocationViewState extends State<SaveLocationView> {
         port.sendPort, LocationServiceRepository.isolateName);
     port.listen(
       (dynamic data) {
-        // Provider.of<SaveLocationViewProvider>(context, listen: false)
-        //     .updateUI(data);
+        Provider.of<SaveLocationViewProvider>(context, listen: false)
+            .updateUI(data);
       },
     );
     Provider.of<SaveLocationViewProvider>(context, listen: false)
@@ -53,7 +53,7 @@ class _SaveLocationViewState extends State<SaveLocationView> {
         .onStart(context);
   }
 
-  /*  @override
+  @override
   void dispose() {
     final saveLocationViewProvider =
         Provider.of<SaveLocationViewProvider>(context, listen: false);
@@ -61,7 +61,7 @@ class _SaveLocationViewState extends State<SaveLocationView> {
     saveLocationViewProvider.destinationController.clear();
     saveLocationViewProvider.sourceController.clear();
     super.dispose();
-  } */
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,148 +89,152 @@ class _SaveLocationViewState extends State<SaveLocationView> {
                 Positioned(
                   top: 150.h,
                   child: Center(
-                    child: Container(
-                      padding: EdgeInsets.only(
-                          left: 30.sp, right: 20.sp, top: 20.sp),
-                      width: MediaQuery.of(context).size.width,
-                      height: 780.h,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30.sp),
-                              topRight: Radius.circular(30.sp))),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            AppLocalization.of(context)!.translate('heading'),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: CustomColor.primaryColor,
-                              fontSize: 22.sp,
-                              fontFamily: FontFamliyM.ROBOTOBOLD,
+                    child: SingleChildScrollView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            left: 30.sp, right: 20.sp, top: 20.sp),
+                        width: MediaQuery.of(context).size.width,
+                        height: 780.h,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30.sp),
+                                topRight: Radius.circular(30.sp))),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              AppLocalization.of(context)!.translate('heading'),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: CustomColor.primaryColor,
+                                fontSize: 22.sp,
+                                fontFamily: FontFamliyM.ROBOTOBOLD,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 30.h,
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding:
-                                    EdgeInsets.symmetric(horizontal: 15.sp),
-                                child: SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width / 1.3,
-                                  child: BuildTextFormField(
-                                    controller: saveLocationViewProvider
-                                        .savePointDestinationController,
-                                    isObserve: false,
-                                    txtHint: AppLocalization.of(context)!
-                                        .translate('hint-title'),
+                            SizedBox(
+                              height: 30.h,
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 15.sp),
+                                  child: SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width / 1.3,
+                                    child: BuildTextFormField(
+                                      controller: saveLocationViewProvider
+                                          .savePointDestinationController,
+                                      isObserve: false,
+                                      txtHint: AppLocalization.of(context)!
+                                          .translate('hint-title'),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 30.h,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-                            child: Column(
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    CircleAvatar(
-                                        backgroundColor: CustomColor.white,
-                                        child: Image.asset(AssetsUtils.source)),
-                                    SizedBox(
-                                      width: 10.w,
-                                    ),
-                                    SizedBox(
-                                      width: 250.w,
-                                      child: BuildTextFormField(
-                                        readOnly: true,
-                                        controller: saveLocationViewProvider
-                                            .sourceController,
-                                        isObserve: false,
-                                        txtHint: AppLocalization.of(context)!
-                                            .translate('hint-source'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20.sp),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      CircleAvatar(
+                                          backgroundColor: CustomColor.white,
+                                          child:
+                                              Image.asset(AssetsUtils.source)),
+                                      SizedBox(
+                                        width: 10.w,
                                       ),
-                                    )
-                                  ],
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.all(20.0.sp),
-                                  child: Container(
-                                    height: 40.h,
-                                    decoration: BoxDecoration(
-                                      border: RDottedLineBorder(
-                                        left: const BorderSide(
-                                          color: Colors.grey,
+                                      SizedBox(
+                                        width: 250.w,
+                                        child: BuildTextFormField(
+                                          readOnly: true,
+                                          controller: saveLocationViewProvider
+                                              .sourceController,
+                                          isObserve: false,
+                                          txtHint: AppLocalization.of(context)!
+                                              .translate('hint-source'),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(20.0.sp),
+                                    child: Container(
+                                      height: 40.h,
+                                      decoration: BoxDecoration(
+                                        border: RDottedLineBorder(
+                                          left: const BorderSide(
+                                            color: Colors.grey,
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    CircleAvatar(
-                                        backgroundColor: CustomColor.white,
-                                        child: Image.asset(
-                                            AssetsUtils.destination)),
-                                    SizedBox(
-                                      width: 10.w,
-                                    ),
-                                    SizedBox(
-                                      width: 250.w,
-                                      child: placesAutoCompleteTextField(
-                                          saveLocationViewProvider
-                                              .destinationController),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      CircleAvatar(
+                                          backgroundColor: CustomColor.white,
+                                          child: Image.asset(
+                                              AssetsUtils.destination)),
+                                      SizedBox(
+                                        width: 10.w,
+                                      ),
+                                      SizedBox(
+                                        width: 250.w,
+                                        child: placesAutoCompleteTextField(
+                                            saveLocationViewProvider
+                                                .destinationController),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 30.h,
-                          ),
-                          saveLocationViewProvider.isSaving
-                              ? AppButton(
-                                  height: 50.sp,
-                                  mycolor: CustomColor.primaryColor,
-                                  sizes: 20.sp,
-                                  text: AppLocalization.of(context)!
-                                      .translate('save-button2'),
-                                )
-                              : GestureDetector(
-                                  onTap: () async {
-                                    await saveLocationViewProvider
-                                        .saveLocationToCollection(
-                                            saveLocationViewProvider
-                                                .savePointDestinationController
-                                                .text,
-                                            saveLocationViewProvider
-                                                .sourceController.text,
-                                            saveLocationViewProvider
-                                                .destinationController.text,
-                                            context);
-                                  },
-                                  child: AppButton(
-                                    mycolor: CustomColor.primaryColor,
+                            SizedBox(
+                              height: 30.h,
+                            ),
+                            saveLocationViewProvider.isSaving
+                                ? AppButton(
                                     height: 50.sp,
+                                    mycolor: CustomColor.primaryColor,
                                     sizes: 20.sp,
                                     text: AppLocalization.of(context)!
-                                        .translate('save-button'),
+                                        .translate('save-button2'),
+                                  )
+                                : GestureDetector(
+                                    onTap: () async {
+                                      await saveLocationViewProvider
+                                          .saveLocationToCollection(
+                                              saveLocationViewProvider
+                                                  .savePointDestinationController
+                                                  .text,
+                                              saveLocationViewProvider
+                                                  .sourceController.text,
+                                              saveLocationViewProvider
+                                                  .destinationController.text,
+                                              context);
+                                    },
+                                    child: AppButton(
+                                      mycolor: CustomColor.primaryColor,
+                                      height: 50.sp,
+                                      sizes: 20.sp,
+                                      text: AppLocalization.of(context)!
+                                          .translate('save-button'),
+                                    ),
                                   ),
-                                ),
-                          //Expanded(child: ListViewWidget())
-                        ],
+                            //Expanded(child: ListViewWidget())
+                          ],
+                        ),
                       ),
                     ),
                   ),
